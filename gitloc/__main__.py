165591071db2908eaab8294ddba56f66dd8201b2
LIU Dongyuan / 柳东原 <liu.dongyuan@gmail.com>
2016-01-05 03:26:05 -0800

    Tally insertions and deletions in 2015

--- gitloc/__main__.py
+++ gitloc/__main__.py
@@ -2,9 +2,10 @@
 """The main entry point. Invoke as `gitloc' or `python -m gitloc'.
 """
 import sys
+from argparse import ArgumentParser
+from datetime import datetime
 
 import pyprind
-from argparse import ArgumentParser
 from pygit2 import init_repository
 from pygit2 import GIT_SORT_TOPOLOGICAL, GIT_SORT_REVERSE
 
@@ -22,20 +23,38 @@ def main():
     commits = list(repo.walk(repo.head.target, GIT_SORT_TOPOLOGICAL | GIT_SORT_REVERSE))
     bar = pyprind.ProgBar(len(commits))
 
+    total_insertions = 0
+    total_deletions = 0
+
     for commit in commits:
         # Skip merge commits
         if len(commit.parents) > 1:
             bar.update()
             continue
 
-        short_message = commit.message.split('\n')[0]
+        # Skip commits not in 2015
+        year = datetime.fromtimestamp(commit.commit_time).year
+        if year != 2015:
+            bar.update()
+            continue
+
+        if len(commit.parents) == 1:
+            diff = repo.diff(commit.parents[0], commit)
+        else:
+            diff = commit.tree.diff_to_tree(swap=True)
+
+        total_insertions += diff.stats.insertions
+        total_deletions += diff.stats.deletions
+
         if args.verbose:
+            short_message = commit.message.split('\n')[0]
             print '[' + commit.author.name + ']', short_hash(commit), short_message
-        diff = repo.diff(commit)
-        if args.verbose:
             print '+' + str(diff.stats.insertions), '-' + str(diff.stats.deletions)
 
         bar.update()
+
+    print '+++ ' + str(total_insertions)
+    print '--- ' + str(total_deletions)
 
 
 if __name__ == '__main__':
